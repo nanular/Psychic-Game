147770215d53dab0c7dd8861b092e4d92afd66d7
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-11-21 19:13:44 -0600

    rehauled code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,75 @@
 $(document).ready(function()
 {
 
+const guessesAllowed = 9;
+var computerChoice = "";
 var totalWins = 0;
 var totalLosses = 0;
-var guessCounter = 9;
-
-var alphabet =
-[
-"a", "b", "c", "d", "e", "f", "g",
-"h", "i", "j", "k", "l", "m", "n",
-"o", "p", "q", "r", "s", "t", "u",
-"v", "w", "x", "y", "z"
-];
-
-var computerChoice = "";
-var userGuess = "";
 var userGuessArray = [];
-var numSameGuesses = 0;
 
-
+roundReset();
 window.addEventListener("keypress", isGuessCorrect, false);
 
-computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log("Answer: " + computerChoice);
 
-function isGuessCorrect(key)
+function fetchRandomLetter()
 {
-	userGuess = String.fromCharCode(key.keyCode);
-	var ASCIIcode = userGuess.charCodeAt(0);
-	if(userGuess === userGuess.toUpperCase())
-	{
-		userGuess = userGuess.toLowerCase();
-	}
-
-	if(ASCIIcode < 65 || (ASCIIcode > 90 && ASCIIcode < 97) || ASCIIcode > 122)
-	{
-		return;
-	}
-
-	console.log(userGuess);
-	
-
-	if (userGuess === computerChoice)
-	{
-		alert("You guessed the correct letter!");
-		totalWins++;
-		guessCounter = 9;
-		userGuessArray = [];
-		userGuess = "";
-		numSameGuesses = 0;
-		document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
-		document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessCounter;
-		document.getElementById("wins").innerHTML = "Total Wins: " + totalWins;
-		computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-		console.log("Answer: " + computerChoice);
-	}
-
-	else if (guessCounter > 1)
-	{
-		
-		if (userGuessArray.indexOf(" " + userGuess) >= 0 && guessCounter !== 9)
-		{
-			alert("You've already guessed " + userGuess + " for this round. Please choose a new letter.");
-			numSameGuesses++;
-
-			if (numSameGuesses > 2)
-			{
-				alert("Alright wise guy.  Let's focus here.  Try guessing a NEW letter.");
-				numSameGuesses = 0;
-			}
-
-		}
-
-		else
-		{
-			guessCounter--;
-			userGuessArray.push(" " + userGuess);
-			document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
-			document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessCounter;
-		}
-	}
-	
-	else
-	{
-		totalLosses++;
-		guessCounter = 9;
-		userGuessArray = [];
-		userGuess = "";
-		numSameGuesses = 0;
-		document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
-		document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessCounter;
-		document.getElementById("losses").innerHTML = "Total Losses: " + totalLosses;
-		computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-		console.log("Answer: " + computerChoice);
-	}
+	var randomChar = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+	console.log("Answer: " + String.fromCharCode(randomChar) + " (" + randomChar + ")");
+	return randomChar; 
 }
 
 
+function roundReset()
+{
+	guessesRemaining = guessesAllowed;
+	userGuessArray.length = 0;
+	document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
+	document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessesRemaining;
+	document.getElementById("wins").innerHTML = "Total Wins: " + totalWins;
+	document.getElementById("losses").innerHTML = "Total Losses: " + totalLosses;
+	computerChoice = fetchRandomLetter();
+}
+
+
+function isGuessCorrect(key)
+{
+	var userGuessKey = key.keyCode;
+	var userGuess = String.fromCharCode(key.keyCode);
+	userGuess = userGuess.toUpperCase();
+	var ASCIIcode = userGuess.charCodeAt(0);
+
+	if (ASCIIcode < 65 || ASCIIcode > 90)
+		{ return; }
+
+	console.log(userGuess + ": " + ASCIIcode);
+
+	if (ASCIIcode === computerChoice)
+	{
+		totalWins++;
+		alert("You guessed the correct letter!");
+		roundReset();
+	}
+
+	else if (userGuessArray.indexOf(" " + userGuess) >= 0 && guessesRemaining !== guessesAllowed)
+	{
+		alert("You've already guessed " + userGuess + " for this round. Please choose a new letter.");
+		return;
+	}
+
+	else if (guessesRemaining > 1)
+	{
+		guessesRemaining--;
+		userGuessArray.push(" " + userGuess);
+		document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
+		document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessesRemaining;
+	}
+
+	else
+	{
+		alert("I'm sorry.  The letter was " + userGuess + ".  Let's try another round!")
+		totalLosses++;
+		roundReset();
+	}
+}
 
 })
---
+++ assets/javascript/oldversion.js
@@ -0,0 +1,101 @@
+$(document).ready(function()
+{
+
+var totalWins = 0;
+var totalLosses = 0;
+var guessCounter = 9;
+
+var alphabet =
+[
+"a", "b", "c", "d", "e", "f", "g",
+"h", "i", "j", "k", "l", "m", "n",
+"o", "p", "q", "r", "s", "t", "u",
+"v", "w", "x", "y", "z"
+];
+
+var computerChoice = "";
+var userGuess = "";
+var userGuessArray = [];
+var numSameGuesses = 0;
+
+
+window.addEventListener("keypress", isGuessCorrect, false);
+
+computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log("Answer: " + computerChoice);
+
+function isGuessCorrect(key)
+{
+	userGuess = String.fromCharCode(key.keyCode);
+	var ASCIIcode = userGuess.charCodeAt(0);
+	if(userGuess === userGuess.toUpperCase())
+	{
+		userGuess = userGuess.toLowerCase();
+	}
+
+	if(ASCIIcode < 65 || (ASCIIcode > 90 && ASCIIcode < 97) || ASCIIcode > 122)
+	{
+		return;
+	}
+
+	console.log(userGuess);
+	
+
+	if (userGuess === computerChoice)
+	{
+		alert("You guessed the correct letter!");
+		totalWins++;
+		guessCounter = 9;
+		userGuessArray = [];
+		userGuess = "";
+		numSameGuesses = 0;
+		document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
+		document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessCounter;
+		document.getElementById("wins").innerHTML = "Total Wins: " + totalWins;
+		computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+		console.log("Answer: " + computerChoice);
+	}
+
+	else if (guessCounter > 1)
+	{
+		
+		if (userGuessArray.indexOf(" " + userGuess) >= 0)
+		{
+			alert("You've already guessed " + userGuess + " for this round. Please choose a new letter.");
+			numSameGuesses++;
+
+			if (numSameGuesses > 2)
+			{
+				alert("Alright wise guy.  Let's focus here.  Try guessing a NEW letter.");
+				numSameGuesses = 0;
+			}
+
+		}
+
+		else
+		{
+			guessCounter--;
+			userGuessArray.push(" " + userGuess);
+			document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
+			document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessCounter;
+		}
+	}
+	
+	else
+	{
+		totalLosses++;
+		guessCounter = 9;
+		userGuessArray = [];
+		userGuess = "";
+		numSameGuesses = 0;
+		document.getElementById("guesses").innerHTML = "Letters Guessed: " + userGuessArray;
+		document.getElementById("guessesRemain").innerHTML = "Guesses Remaining: " + guessCounter;
+		document.getElementById("losses").innerHTML = "Total Losses: " + totalLosses;
+		computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+		console.log("Answer: " + computerChoice);
+	}
+}
+
+
+
+})
